619aa749102c3d4852a690f2bb08c40efd811014
Franklin Obasi <franklinobasy@gmail.com>
2022-11-16 21:08:39 +0100

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,50 +1,27 @@
 #!/usr/bin/python3
 '''
-Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-
-Requirements:
-
-1. You must use urllib or requests module
-2. The script must accept an integer as a parameter, which is the employee ID
-3. The script must display on the standard output the
-   employee TODO list progress in this exact format:
-    - First line: Employee EMPLOYEE_NAME is done with tasks
-      (NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-        a. EMPLOYEE_NAME: name of the employee
-        b. NUMBER_OF_DONE_TASKS: number of completed tasks
-        c. TOTAL_NUMBER_OF_TASKS: total number of tasks,
-           which is the sum of completed and non-completed tasks
-    - Second and N next lines display the title of completed tasks: TASK_TITLE
-        (with 1 tabulation and 1 space before the TASK_TITLE)
+Python script that returns information using REST API
 '''
-
 import requests
-import sys
+from sys import argv
 
-
-if (__name__ == "__main__") and (len(sys.argv) > 1):
-    id = int(sys.argv[1])
-    url = "https://jsonplaceholder.typicode.com"
-    users = requests.get(f"{url}/users").json()
-    todos = requests.get(f"{url}/todos").json()
-
-    n_todos = 0
-    n_tasks_completed = 0
-    tasks_completed_title = []
-
-    for todo in todos:
-        if todo["userId"] == id:
-            n_todos += 1
-            if (todo["completed"]):
-                n_tasks_completed += 1
-                tasks_completed_title.append(todo["title"])
-
-    for user in users:
-        if user["id"] == id:
-            text1 = f"Employee {user['name']} is done "
-            text2 = f"with tasks({n_tasks_completed}/{n_todos}):"
-            print(text1 + text2)
-            for title in tasks_completed_title:
-                print(f"\t {title}")
-            break
+if __name__ == "__main__":
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
