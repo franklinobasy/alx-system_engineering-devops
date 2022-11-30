441d7015beb3376c5a491afc2cb7858f33c9e7be
Franklin Obasi <franklinobasy@gmail.com>
2022-11-30 14:34:39 +0100

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,8 @@
 '''
 Write a function that queries the Reddit API and returns the
 number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given, the function should return 0.
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
 
 Hint: No authentication is necessary for most features of the Reddit API.
 If you're getting errors related to Too Many Requests,
